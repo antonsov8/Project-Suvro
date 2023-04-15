363aa140ad6ee49cb7b7f452ffd8ec46718cbd2b
Anton <antonsov8@gmail.com>
2023-04-15 11:57:08 +0530

    Made a fully functioning parallelised eigen value calculator. Now need to make the correspoding qsub file and upload on github

--- 20230415_Suvro_Eigen.R
+++ 20230415_Suvro_Eigen.R
@@ -2,10 +2,10 @@ library(doParallel)
 library(foreach)
 
 # Part1: Creating Matrices ------------------------------------------------
+rm(list = ls())
 
 
-
-Suvro__func <- 
+Suvro_func <- 
   function(n = 2){
   sample.int(100, size =1000000*n*n, replace = T) |>
     matrix(nrow = 1000*n, ncol = 1000*n) |> 
@@ -14,22 +14,22 @@ Suvro__func <-
 }
 
 # Suvro__func()
-
-
 # Part2: Cluster ----------------------------------------------------------
+Suvro__env <- ls() 
 
-
-
-Suvro__myCluster <- makeCluster(10, type = "PSOCK")
-register(Suvro__myCluster)
-
+Suvro__myCluster <- makeCluster(5, type = "PSOCK")
+registerDoParallel(Suvro__myCluster)
 
 # Part3: For Loop ---------------------------------------------------------
 
-
-foreach(1:10,
-        .export = ,
-        .packages = c("doParallel")) %dopar% {} 
+Suvro_data <- 
+  foreach(parIter_i = 1:10,
+        .combine = cbind,
+        .export = Suvro__env,
+        .packages = c("doParallel")) %dopar% {
+          return(Suvro_func(n = 1/parIter_i))
+          
+        } 
 
 
 # Part4: Stop Cluster -----------------------------------------------------
